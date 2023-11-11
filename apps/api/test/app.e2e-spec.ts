f85830d5f2796ed8ac8468398a78621e671f1674
Piotr Graczyk <goldlightdrake@gmail.com>
2023-11-11 22:23:31 +0100

    feat(api): add e2e test for api

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('NotesController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,51 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/notes (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/notes')
+      .send({ title: 'Test Note', content: 'This is a test note.' })
+      .expect(201);
+  });
+
+  it('/notes (GET)', () => {
+    return request(app.getHttpServer()).get('/notes').expect(200);
+  });
+
+  it('/notes/:id (GET)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .send({ title: 'Test Note', content: 'This is a test note.' })
+      .expect(201);
+
+    const noteId = response.body.id;
+
+    return request(app.getHttpServer()).get(`/notes/${noteId}`).expect(200);
+  });
+
+  it('/notes/:id (PATCH)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .send({ title: 'Test Note', content: 'This is a test note.' })
+      .expect(201);
+
+    const noteId = response.body.id;
+    return request(app.getHttpServer())
+      .patch(`/notes/${noteId}`)
+      .send({
+        title: 'Updated Test Note',
+        content: 'This is an updated test note.',
+      })
+      .expect(200);
+  });
+
+  it('/notes/:id (DELETE)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/notes')
+      .send({ title: 'Test Note', content: 'This is a test note.' })
+      .expect(201);
+
+    const noteId = response.body.id;
+    return request(app.getHttpServer()).delete(`/notes/${noteId}`).expect(204);
   });
 });
