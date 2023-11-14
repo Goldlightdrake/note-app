e9d4b9d353f41b1dc52ab101af0f41f3c35053a9
Piotr Graczyk <goldlightdrake@gmail.com>
2023-11-14 01:09:28 +0100

    feat(api): add rate limiter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    NotesModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
